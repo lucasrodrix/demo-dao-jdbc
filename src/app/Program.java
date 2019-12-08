9c36a04ca5a98c5432ade5165fba49f865117f01
Lucas Rodrix <lucas@gruporodrix.net>
2019-12-08 18:29:26 -0300

    Creating project and git repository

---
+++ src/app/Program.java
@@ -0,0 +1,14 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+	}
+}
