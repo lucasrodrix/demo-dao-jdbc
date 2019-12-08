dd63c0200089da7eabeb6bb1f1969efd2e9ab5f1
Lucas Rodrix <lucas@gruporodrix.net>
2019-12-08 18:44:14 -0300

    Department entity class

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,14 +1,11 @@
 package app;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 	}
 }
